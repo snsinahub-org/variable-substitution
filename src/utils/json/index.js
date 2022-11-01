549d1183c2944babbe5ecc9f9afd81ad9cacf4ea
root <root@localhost.localdomain>
2022-11-01 00:49:02 -0400

    add

--- src/utils/json/index.js
+++ src/utils/json/index.js
@@ -24,7 +24,7 @@ module.exports = class JsonVarSub {
 
         
             console.log("write to file: ", writeToFile)
-            fs.appendFileSync(outputFile, JSON.stringify(modifiedJson));
+            fs.appendFileSync(outputFile, modifiedJson);
         
         
         return modifiedJson
