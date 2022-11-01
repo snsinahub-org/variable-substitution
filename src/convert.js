4a4123b34504b482a31b31263f426333823bc281
root <root@localhost.localdomain>
2022-11-01 01:00:01 -0400

    add

--- src/convert.js
+++ src/convert.js
@@ -25,7 +25,7 @@ async function run() {
     const octokit = github.getOctokit(myToken)
     
     
-    fs.appendFileSync(process.env.GITHUB_OUTPUT, "subbed=" + subbed);
+    fs.appendFileSync(process.env.GITHUB_OUTPUT, "subbed=" + JSON.parse(subbed));
 }
 
 run();
