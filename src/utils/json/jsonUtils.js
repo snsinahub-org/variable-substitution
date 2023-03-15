94314ad8025e0d3a84c59de43bad8697e9e865d6
snsinahub <snsinahub@github.com>
2023-03-15 15:59:21 -0400

    modify code

--- src/utils/json/jsonUtils.js
+++ src/utils/json/jsonUtils.js
@@ -61,7 +61,7 @@ module.exports = class JsonUtils {
                 }
                 
             } else {              
-                console.log("KEY -- ELSE ", key, element, keyName, matchingKey, matchingValue, updatingKey, value)                                  
+                console.log("KEY -- ELSE ", key, element, keyName, matchingKey, matchingValue, updatingKey, value, keyValue)                                  
                 if(keyName == '') {
                     // console.log("KEY -- ELSE MATCHED >>>>>>>>>>", key, keyName, matchingKey, matchingValue, updatingKey, value)
                     if(element == `${key}`) {
