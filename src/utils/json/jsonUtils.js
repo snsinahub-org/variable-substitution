204493ec59ee49850d73ced374049ec106b73de8
snsinahub <snsinahub@github.com>
2023-03-15 14:39:28 -0400

    modify code

--- src/utils/json/jsonUtils.js
+++ src/utils/json/jsonUtils.js
@@ -38,16 +38,19 @@ module.exports = class JsonUtils {
         return jsonObject;       
     }
 
-    printObjectReplaceKeyBased(jsonObject, keyName = '', element = '', theKey = '', keyValue = '', delimiter = '/') {
+    printObjectReplaceKeyBased(jsonObject, keyName = '', element = '', matchingKey = '', updatingKey = '', keyValue = '', delimiter = '/') {
         let self = this;
         _.forEach(jsonObject, function(value, key){
             
             if(typeof jsonObject[key] === "object") { 
                 if(keyName == element) {
+                    for(let k in jsonObject[key]) {
+                        console.log("ELEMENT KEY == ", k, matchingKey, updatingKey, value, jsonObject[key][k])
+                    }
                     console.log("ELEMENT OBJECT == ", key, keyName, JSON.stringify(jsonObject[key]))
                 } 
-                console.log("KEY -- OBJECT", key, element, theKey, keyValue, keyName)                   
-                self.printObjectReplaceKeyBased(jsonObject[key], self._createKeyName(keyName, key, delimiter), element, theKey, keyValue, delimiter);
+                console.log("KEY -- OBJECT", key, element, matchingKey, keyValue, keyName)                   
+                self.printObjectReplaceKeyBased(jsonObject[key], self._createKeyName(keyName, key, delimiter), element, matchingKey, keyValue, delimiter);
             } else {              
                                                 
                 if(keyName == '') {
