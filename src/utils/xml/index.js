75ad1f5c71191da6cb177592b9d5d018ee6b2c12
Siavash Namvar <snsinahub@github.com>
2024-02-23 10:31:32 -0500

    print variables

--- src/utils/xml/index.js
+++ src/utils/xml/index.js
@@ -32,13 +32,7 @@ module.exports = class XmlVarSub {
         
 
         for(let i = 0; i < variables.length; i++ ){
-            if(variables[i]['value'] == '' && skipeEmpty) {
-                // modifiedJson = variables[i];
-                // console.log("INNER modifiedJson: >>>>> ", modifiedJson)
-                // console.log("INNER VARIABLE: >>>>> ", variables[i])
-            } else {
-                modifiedJson = jUtils.printObjectReplaceKeyBased(jsonObj, '', variables[i]['element'], variables[i]['matchingKey'], variables[i]['matchingValue'], variables[i]['updatingKey'] ,variables[i]['value'], delimiter);
-            }
+            modifiedJson = jUtils.printObjectReplaceKeyBased(jsonObj, '', variables[i]['element'], variables[i]['matchingKey'], variables[i]['matchingValue'], variables[i]['updatingKey'] ,variables[i]['value'], delimiter, skipeEmpty);
             console.log("modifiedJson: ", modifiedJson)
         }
         
