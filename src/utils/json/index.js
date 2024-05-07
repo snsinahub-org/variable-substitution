2bcee08f6e28f4c2e403da9985ee1d7cc98c95bf
Siavash Namvar <snsinahub@github.com>
2024-05-07 08:24:12 -0400

    add json5

--- src/utils/json/index.js
+++ src/utils/json/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const _ = require('lodash')
 const JsonUtils = require('./jsonUtils.js')
 const { TextDecoder } = require('node:util');
+const json5 = require('json5');
 
 
 module.exports = class JsonVarSub {
@@ -16,7 +17,15 @@ module.exports = class JsonVarSub {
         let jsonObject = JSON.parse(new TextDecoder('utf-8', { ignoreBOM: false}).decode(rawData));
         let jUtils = new JsonUtils()
         let modifiedJson = jsonObject;
-        let variables = JSON.parse(vars)
+        // let variables = JSON.parse(vars)
+
+        try {
+            let variables = json5.parse(vars);
+        } catch (error) {
+            console.error("Parsing error:", error);
+            console.log(vars)
+            // Handle or ignore the error as needed
+        }
 
         for(let i = 0; i < variables.length; i++ ){
             console.log("Variables: ", variables[i], variables[i]['key'], variables[i]['value'])
